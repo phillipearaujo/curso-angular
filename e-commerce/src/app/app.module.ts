187133ad4ddfe064e3fdab2d65b5110b4e315b63
phillipearaujo <araujophill@gmail.com>
2023-03-10 11:39:12 -0300

    refactor project e-commerce

--- e-commerce/src/app/app.module.ts
+++ e-commerce/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule} from "@angular/material/snack-bar";
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
