a8be177f197304d248658b1cb10b7258113d8e4f
phillipearaujo <araujophill@gmail.com>
2023-03-10 22:38:16 -0300

    refactor: project e-commerce

--- e-commerce/src/app/app-routing.module.ts
+++ e-commerce/src/app/app-routing.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CarrinhoRoutingModule } from './carrinho/carrinho-routing.module';
-import { CarrinhoComponent } from './carrinho/carrinho.component';
 import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 
 const routes: Routes = [
   { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
   { path: "", redirectTo: "produtos", pathMatch: "full" },
-  { path: "**", component: NaoEncontradoComponent},
-  { path: 'carrinho', component: CarrinhoComponent}
+  { path: "**", component: NaoEncontradoComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), CarrinhoRoutingModule],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
